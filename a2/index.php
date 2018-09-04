b3f5bff40d2790984203a15cb9c0270cd404e679
Jono Diver <s3707275@student.rmit.edu.au>
2018-09-04 12:27:01 +1000

    homepage grid layout

--- a2/index.php
+++ a2/index.php
@@ -31,42 +31,63 @@
                 <area shape="rect" coords="0,0,450,200" alt="PTB logo" href="index.php">
             </map>
         </header>
+
         <main>
+            <div class="grid-container">
 
-            <h2 class="areatitle">MELBOURNE</h2>
+                <div class="item1">
+                    <h2 class="areatitle">MELBOURNE</h2>
+                    <p> Come fly with us over the beautiful Melbourne skyline and enjoy views such as; the MCG, Eureka Tower and RMIT University.</p>
+                </div>
 
-            <div class="picturebox">
-                <img class="photolinks" src='../../media/PTB/Melbourne.jpeg' alt='Melbourne Ballooning Picture/Link' />
-                <a href="melbourne.php">
-                    <div class="phototext">
-                        <div class="text">Melbourne Flights</div>
+                <div class="item2">
+                    <div class="picturebox">
+                        <img class="photolinks" src='../../media/PTB/Melbourne.jpeg' alt='Melbourne Ballooning Picture/Link' />
+                        <a href="melbourne.php">
+                            <div class="phototext">
+                                <div class="text">Melbourne Flights</div>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-            <p> Come fly with us over the beautiful Melbourne skyline and enjoy views such as; the MCG, Eureka Tower and RMIT University.</p>
+                </div>
 
-            <h2 class="areatitle">YARRA VALLEY</h2>
-            <div class="picturebox">
-                <img class="photolinks" src='../../media/PTB/YarraValley.jpeg' alt='Yarra Valley Ballooning Picture/Link' />
-                <a href="yarra_valley.php">
-                    <div class="phototext">
-                        <div class="text">Yarra Valley Flights</div>
+                <div class="item3">
+                    <div class="picturebox">
+                        <img class="photolinks" src='../../media/PTB/YarraValley.jpeg' alt='Yarra Valley Ballooning Picture/Link' />
+                        <a href="yarra_valley.php">
+                            <div class="phototext">
+                                <div class="text">Yarra Valley Flights</div>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-            <p>The most iconic way to the see the stunning Yarra Valley, watch the sunrise over lucious vineyards and farms. Who knows, you might even see a kangaroo too!</p>
+                </div>
 
+                <div class="item4">
+                    <h2 class="areatitle">YARRA VALLEY</h2>
+                    <p>The most iconic way to the see the stunning Yarra Valley, watch the sunrise over lucious vineyards and farms. Who knows, you might even see a kangaroo too!</p>
+                </div>
 
-            <h2 class="areatitle">AERIAL ADVERTISING</h2>
-            <div class="picturebox">
-                <img class="photolinks" src='../../media/PTB/footy.jpg' alt='Carlton Pot Special Shape Balloon over MCG' />
-                <a href="advertising.php">
-                    <div class="phototext">
-                        <div class="text">Aerial Advertising</div>
+                <div class="item5">
+                    <h2 class="areatitle">AERIAL ADVERTISING</h2>
+                    <p>Looking to advertise your company in an original, exciting way? Take advantage of our special shape balloons to promote your business. We offer a range of services to cater to all businesses needs, find out how we can help elevate your business up, up and away!</p>
+                </div>
+
+                <div class="item6">
+                    <div class="picturebox">
+                        <img class="photolinks" src='../../media/PTB/footy.jpg' alt='Carlton Pot Special Shape Balloon over MCG' />
+                        <a href="advertising.php">
+                            <div class="phototext">
+                                <div class="text">Aerial Advertising</div>
+                            </div>
+                        </a>
                     </div>
-                </a>
+                </div>
             </div>
-            <p>Looking to advertise your company in an original, exciting way? Take advantage of our special shape balloons to promote your business. We offer a range of services to cater to all businesses needs, find out how we can help elevate your business up, up and away!</p>
+
+            <div id="phonenumber">
+                <h1 id="phone">Call us on (03) 9429 0277 or send <br />an email to info@picturethisballooning.com.au</h1>
+            </div>
+
 
         </main>
 
