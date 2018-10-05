d6577f633a65be07986fe297fa80b1cf868212bc
Jono Diver <s3707275@student.rmit.edu.au>
2018-10-05 14:30:13 +1000

    top_module and end_module present and functioning on all pages

--- a3/login.php
+++ a3/login.php
@@ -1,80 +1,26 @@
 <?php
 session_start();
+$page_title = 'index';
 include_once('tools.php');
-topModule();
+top_module();
 ?>
-<!DOCTYPE html>
-<html lang='en'>
 
-<head>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-    <meta charset="utf-8">
-    <title>Assignment 2</title>
-
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css">
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-    <script src='../wireframe.js'></script>
-</head>
-
-<body>
-    <nav>
-        <ul class="navbar">
-            <li class="navelement"><a class="navlink" href="index.php">Home</a></li>
-            <li class="navelement"><a class="navlink" href="services.php">Services</a></li>
-            <li class="navelement"><a class="navlink" href="melbourne.php">Melbourne Flights</a></li>
-            <li class="navelement"><a class="navlink" href="yarra_valley.php">Yarra Valley Flights</a></li>
-            <li class="navelement"><a class="navlink" href="advertising.php">Aerial Advertising</a></li>
-            <li class="navelement"><a class="navlink" href="login.php">Login</a></li>
-        </ul>
-    </nav>
-    <div id="mainbody">
-        <header>
-            <img id="headerphoto" src="../../media/PTB/header.png" alt="Panorama of hot air balloons in flight, with company logo" usemap="logomap" />
-            <!-- https://www.wowamazing.com/talent/photography/the-most-stunning-photos-of-hot-air-balloon-festivals-from-all-over-the-world/ -->
-            <map name="logomap">
-                <area shape="rect" coords="0,0,450,200" alt="PTB logo" href="index.php">
-            </map>
-        </header>
-        <main>
-
-            <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php" method="post">
-                <div class="imgcontainer">
-                    <img src="../../media/PTB/login%20avatar.jpg" alt="Jono Diver holding the balloon from flying away" class="avatar">
-                    <!-- Taken during my first shift with PTB -->
-                </div>
-
-                <div class="formcontainer">
-                    <label for="email"><b>Username</b></label>
-                    <input type="email" placeholder="Enter Email" name="email" required>
-
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="password" required>
-
-                    <button id="submit" type="submit">Login</button>
-                </div>
-
-            </form>
-
-
-
-        </main>
-
-        <footer>
-            <div>&copy;<script>
-                    document.write(new Date().getFullYear());
-
-                </script> Jonathan Diver, s3707275.</div>
-            <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-            <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-        </footer>
+<form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php" method="post">
+    <div class="imgcontainer">
+        <img src="../../media/PTB/login%20avatar.jpg" alt="Jono Diver holding the balloon from flying away" class="avatar">
+        <!-- Taken during my first shift with PTB -->
     </div>
 
-</body>
-<script>
-    document.getElementById("wireframecss").disabled = true;
+    <div class="formcontainer">
+        <label for="email"><b>Username</b></label>
+        <input type="email" placeholder="Enter Email" name="email" required>
 
-</script>
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
 
-</html>
+        <button id="submit" type="submit">Login</button>
+    </div>
+
+</form>
+
+<?php end_module(); ?>
--- a3/tools.php
+++ a3/tools.php
@@ -1,18 +1,60 @@
 <?php
 
-function topModule(){
-    echo <!DOCTYPE html>;
-    echo <html lang='en'>;
-    echo <head>;
-        echo <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">;
-        echo <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">;
-        echo <link href="https://fonts.googleapis.com/css?family=Caveat"     rel="stylesheet">;
-        echo <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">;
-        echo <meta charset="utf-8">;
-        echo <title>Assignment 3</title>;
-        echo <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css">;
-        echo <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">;
-        echo <script src='../wireframe.js'></script>;
-    echo </head>;
+function top_module() {
+    $html = <<<OUTPUT
+    <!DOCTYPE html>;
+    <html lang='en'>;
+
+    <head>;
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">;
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">;
+        <link href="https://fonts.googleapis.com/css?family=Caveat"     rel="stylesheet">;
+        <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">;
+        <meta charset="utf-8">;
+        <title>Assignment 3</title>;
+        <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css">;
+        <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">;
+        <script src='../wireframe.js'></script>;
+    </head>;
+
+    <body>
+    <nav>
+        <ul class="navbar">
+            <li class="navelement"><a class="navlink" href="index.php">Home</a></li>
+            <li class="navelement"><a class="navlink" href="services.php">Services</a></li>
+            <li class="navelement"><a class="navlink" href="melbourne.php">Melbourne Flights</a></li>
+            <li class="navelement"><a class="navlink" href="yarra_valley.php">Yarra Valley Flights</a></li>
+            <li class="navelement"><a class="navlink" href="advertising.php">Aerial Advertising</a></li>
+            <li class="navelement"><a class="navlink" href="login.php">Login</a></li>
+        </ul>
+    </nav>
+
+    <div id="mainbody">
+        <header>
+            <img id="headerphoto" src="../../media/PTB/header.png" alt="Panorama of hot air balloons in flight, with company logo" usemap="logomap" />
+            <map name="logomap">
+                <area shape="rect" coords="0,0,450,200" alt="PTB logo" href="index.php">
+            </map>
+        </header>
+    <main>
+OUTPUT;
+        echo $html;
+}
+
+function end_module() {
+    $html = <<<OUTPUT
+    </main>
+    <footer>
+        <div>&copy;<script>document.write(new Date().getFullYear());
+        </script> Jonathan Diver, s3707275.</div>
+        <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+        <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+        </footer>
+    </div>
+    </body>
+    <script>document.getElementById("wireframecss").disabled = true;</script>
+    </html>
+OUTPUT;
+        echo $html;
 }
 ?>
