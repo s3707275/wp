a0137c99ea375fb3e4be963277d48fec80f07d9b
Jono Diver <s3707275@student.rmit.edu.au>
2018-10-10 11:50:39 +1100

    session cart added

--- a3/cart.php
+++ a3/cart.php
@@ -18,50 +18,20 @@ echo "<p> id = " . $id . "</p>";
 echo "<p> oid = " . $oid . "</p>";
 echo "<p> qty = " . $qty . "</p>";
 
+// create session array if empty
+if(empty($_SESSION['cart']))
+    $_SESSION['cart'] = array();
 
 if (isset($_POST['add'], $_POST['id'], $_POST['qty'], $_POST['oid'])) {
   // server side code is required here to validate and check if
   //  - qty is a positive integer (ie 1 or more)
   //  - product/service and option ids are valid
-        if ($POST['qty'] > 0){
-
-            if ($POST['id'] == "F100")
-               if ($POST['oid'] == "ALT"){
-                    $_SESSION['cart'][$id]['oid'] = $oid;
-                    $_SESSION['cart'][$id]['qty'] = $qty;
-                }
-                elseif ($POST['oid' == "CLD"]){
-                    $_SESSION['cart'][$id]['oid'] = $oid;
-                    $_SESSION['cart'][$id]['qty'] = $qty;
-                }
-            elseif ($POST['id'] == "F200")
-                if ($POST['oid'] == "ALT"){
-                    $_SESSION['cart'][$id]['oid'] = $oid;
-                    $_SESSION['cart'][$id]['qty'] = $qty;
-                    }
-                elseif ($POST['oid' == "CLD"]){
-                    $_SESSION['cart'][$id]['oid'] = $oid;
-                    $_SESSION['cart'][$id]['qty'] = $qty;
-                }
-            elseif ($POST['id'] == "F300")
-                if ($POST['oid'] == "MOM"){
-                    $_SESSION['cart'][$id]['oid'] = $oid;
-                    $_SESSION['cart'][$id]['qty'] = $qty;
-                }
-                elseif ($POST['oid' == "UNI"]){
-                    $_SESSION['cart'][$id]['oid'] = $oid;
-                    $_SESSION['cart'][$id]['qty'] = $qty;
-                }
-            else
-                echo "<p> invalid - please try again </p>";
         }
 //  $_SESSION['cart'][$id]['oid'] = $oid;
 //  $_SESSION['cart'][$id]['qty'] = $qty;
     echo "<p> session id = " . $_SESSION['cart'][$id]['oid'] . "</p>";
     echo "<p> session oid = " . $_SESSION['cart'][$id]['qty'] . "</p>";
     echo "<p> session qty = " . $_SESSION['cart'][$id] . "</p>";
-
-}
 
 
 
